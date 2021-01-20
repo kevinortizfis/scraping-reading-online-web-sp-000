914f82b062845fed6386c0c75d0fede24cc41a52
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-20 02:10:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,5 +2,5 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-puts doc.css(".headline-26OIBN").text
+doc = Nokogiri::HTML(open("https://twitter.com/home"))
+puts doc.css("css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0").text
